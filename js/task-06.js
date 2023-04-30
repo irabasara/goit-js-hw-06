3c7c9b3e11db1b4c28ce0a3260e00a39e47b0d3b
irabasara <125157770+irabasara@users.noreply.github.com>
2023-04-30 22:08:25 +0300

    task 6

---
+++ js/task-06.js
@@ -0,0 +1,15 @@
+const userInputEl = document.querySelector('#validation-input');
+const lengthOnEl = userInputEl.getAttribute('data-length');
+
+
+const findLengthOnInputEl = (event) => {
+    if (event.currentTarget.value.length >= lengthOnEl) {
+        userInputEl.classList.remove('invalid')
+        userInputEl.classList.add('valid');
+    } else {
+        userInputEl.classList.remove('valid');
+        userInputEl.classList.add('invalid');
+    }
+};
+
+userInputEl.addEventListener('blur', findLengthOnInputEl);
