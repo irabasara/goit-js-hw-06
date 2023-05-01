894fea561ad13e3c342a7a2ebf6292fd2168e083
irabasara <125157770+irabasara@users.noreply.github.com>
2023-05-01 12:56:43 +0300

    task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,24 @@
+const formEl = document.querySelector('.login-form');
+const nameInputEl = document.querySelector('input[name="email"]');
+const namePasswordEl = document.querySelector('input[name="password"]');
+
+
+const OnSubmitClick = event => {
+    event.preventDefault();
+    if (nameInputEl.value === '' || namePasswordEl.value === '') {
+        alert('будь ласка, заповніть усі поля')
+    } else {
+        const emailValue = event.currentTarget.elements.email.value;
+        const passwordValue = event.currentTarget.elements.password.value;
+        console.log(`email: ${emailValue}, password: ${passwordValue}`);
+        event.currentTarget.reset();
+
+    }
+}
+
+
+formEl.addEventListener('submit', OnSubmitClick)
+
+
+
 
